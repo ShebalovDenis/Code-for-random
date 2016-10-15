55ce018e83d67392adec4c510e0657bd496f5d1a
ShebalovDenis <chempikd@yandex.ru>
2016-10-15 17:33:02 +0300

    for VadimbI4

---
+++ RandomMassive.java
@@ -0,0 +1,26 @@
+
+
+public class RandomMassive {
+	
+	/*public static void main(String[] args) {
+		RandomMassive random = new RandomMassive();
+		int[] numbers = new int[100];
+		for(int i = 0; i < numbers.length; i++){
+		    numbers[i] = random.nextInt();
+		    System.out.println(numbers[i]);
+		
+		
+		
+	}
+	}*/
+	public static void main(String[] args){
+	int[] arr = new int[10];{
+	 
+	 for(int i = 0; i < arr.length; i++){
+	   arr[i] = (int)(Math.random()*100); // это случайными 100
+	   System.out.println(arr[i]);
+	}
+	}
+	}
+
+}
